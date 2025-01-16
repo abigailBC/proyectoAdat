33dbd1c3fd9451c5281e1184234ac422d30ddc06
abigailBC <mixolida36@gmail.com>
2025-01-16 19:39:34 +0100

    Actualización Objectdb y menú 2

---
+++ src/Modelo/BasexManager.java
@@ -0,0 +1,19 @@
+package Modelo;
+
+import java.util.HashMap;
+
+public class BasexManager extends Manager {
+
+	@Override
+	public HashMap<String, Libro> recorrer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void guardarLibros(HashMap<String, Libro> libros) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
